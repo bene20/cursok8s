12522033a9778216461cfadf51fa3608fb75e075
Ebenézer Rangel Botelho <ebenezer.botelho@trt10.jus.br>
2020-06-30 17:23:32 -0300

    Ajustes na criação da base de dados e no script de início de trabalho

--- mysql/import.sh
+++ mysql/import.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mysql -u root -pq1w2e3r4 empresa < empresa_noticias.sql
+mysql -h 127.0.0.1 -P 3307 -u root -pq1w2e3r4 empresa < empresa_noticias.sql
 
-mysql -u root -pq1w2e3r4 empresa < empresa_usuario.sql
+mysql -h 127.0.0.1 -P 3307 -u root -pq1w2e3r4 empresa < empresa_usuario.sql
--- startWork.sh
+++ startWork.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-./makeImages.sh
+oldDir=$(pwd)
+workdir=$(realpath $(dirname $0))
 
+echo "workdir=$workdir"
+
+echo "============================================"
+echo "(Re)criando base de dados da aplicação"
+cd $workdir/mysql
+sh import.sh
+
+echo "============================================"
+echo "Gerando imagens das aplicações 'noticia' e 'sistema'"
+cd $workdir
+./makeImages.sh <<EOF
+1
+1
+EOF
+
+echo "============================================"
+echo "Encerrando o Minikube caso esteja de pé..."
 minikube stop
+echo "Excluindo potencial cluster antigo do Minikube..."
 minikube delete
+echo "Carregando o Minikube..."
 minikube start
 
+echo "============================================"
+echo "Criando o cluster K8S..."
 kubectl create -f kubernetes/permissao-imagens.yml 
 kubectl create -f kubernetes/permissao-sessao.yml 
 kubectl create -f kubernetes/deployment-aplicacao.yml 
@@ -15,4 +37,8 @@ kubectl create -f kubernetes/servico-aplicacao-sistema.yml
 kubectl create -f kubernetes/servico-statefulset.yml 
 kubectl create -f kubernetes/statefulset-sistema.yml
 
+echo "============================================"
+echo "Listando os dados de acesso à aplicação que roda no cluster:"
 minikube service list
+
+cd $oldDir
